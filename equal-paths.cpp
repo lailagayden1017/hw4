386d0124bcdc301a2496d318a59c88f0b8857744
Laila Gayden <lgayden@usc.edu>
2025-04-09 00:05:25 +0000

    Second commit for HW4

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,26 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+bool check(Node* root, int depth, int& leaf){
+  if(!root) return true; 
+
+  if(!root->left&& !root->right){
+    if(leaf == -1){
+      leaf = depth; 
+      return true; 
+    }
+    return depth == leaf;
+  }
+  return check(root->left, depth+1, leaf) && check(root->right, depth+1, leaf);
+}
 
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+    int leaf = -1; 
+    return check(root, 0, leaf);
+    
+    
+    
 
 }
-
